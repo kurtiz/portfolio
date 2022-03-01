42e01ee0737ec0cc193e6ddadc1c09cbd280e980
kurtiz <papiliocurtis@gmail.com>
2022-03-01 16:32:59 +0000

    First touch on about section :zap:

--- app/Views/home.php
+++ app/Views/home.php
@@ -89,33 +89,35 @@
                 <img src="<?=base_url();?>/public/img/me.jpg" class="img-fluid" alt="">
             </div>
             <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-                <h3>UI/UX &amp; Graphic Designer</h3>
+                <h3>Aaron Will Djaba</h3>
                 <p class="fst-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua.
+                   I am open minded, docile and versatile. I love sports and video games and I am a Tech Enthusiast.
                 </p>
                 <div class="row">
                     <div class="col-lg-6">
                         <ul>
-                            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                            <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                            <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-6">
-                        <ul>
-                            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                            <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                            <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>September 29th</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span><a href="<?=base_url()?>"><?=base_url()?></a></span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong>
+                                <span>+233 55 065 6272 |</span>
+                                <span> +233 59 598 7451</span>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Pokuase, Accra</span></li>
+
+                            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span><?=(int)date("Y") - 1998?></span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bsc. Computer Engineering</span></li>
+                            <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>aaronwilldjaba@outlook.com</span></li>
                             <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
                         </ul>
                     </div>
                 </div>
+                <?php /*
                 <p>
                     Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
                     Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
                 </p>
+                    */
+                ?>
             </div>
         </div>
 
@@ -129,7 +131,7 @@
             <div class="col-lg-3 col-md-6">
                 <div class="count-box">
                     <i class="bi bi-emoji-smile"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+                    <span data-purecounter-start="0" data-purecounter-end="121" data-purecounter-duration="1" class="purecounter"></span>
                     <p>Happy Clients</p>
                 </div>
             </div>
@@ -153,7 +155,7 @@
             <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
                 <div class="count-box">
                     <i class="bi bi-award"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+                    <span data-purecounter-start="0" data-purecounter-end="0" data-purecounter-duration="1" class="purecounter"></span>
                     <p>Awards</p>
                 </div>
             </div>
@@ -174,21 +176,21 @@
             <div class="col-lg-6">
 
                 <div class="progress">
-                    <span class="skill">HTML <i class="val">100%</i></span>
+                    <span class="skill">Web Development <i class="val">100%</i></span>
                     <div class="progress-bar-wrap">
                         <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
 
                 <div class="progress">
-                    <span class="skill">CSS <i class="val">90%</i></span>
+                    <span class="skill">Graphic Design<i class="val">98%</i></span>
                     <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
 
                 <div class="progress">
-                    <span class="skill">JavaScript <i class="val">75%</i></span>
+                    <span class="skill">Mobile/ Desktop App Development<i class="val">75%</i></span>
                     <div class="progress-bar-wrap">
                         <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -199,23 +201,23 @@
             <div class="col-lg-6">
 
                 <div class="progress">
-                    <span class="skill">PHP <i class="val">80%</i></span>
+                    <span class="skill">Game Development<i class="val">75%</i></span>
                     <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
 
                 <div class="progress">
-                    <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+                    <span class="skill">UI/UX Design<i class="val">98%</i></span>
                     <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
 
                 <div class="progress">
-                    <span class="skill">Photoshop <i class="val">55%</i></span>
+                    <span class="skill">Communication Skills<i class="val">95%</i></span>
                     <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
 
@@ -224,7 +226,7 @@
         </div>
 
     </div><!-- End Skills -->
-
+<?php /*
     <!-- ======= Interests ======= -->
     <div class="interests container">
 
@@ -308,7 +310,8 @@
         </div>
 
     </div><!-- End Interests -->
-
+*/
+ ?>
     <!-- ======= Testimonials ======= -->
     <div class="testimonials container">
 
